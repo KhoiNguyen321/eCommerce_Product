47752c8a51c20938ea6fda19c2461a5d736aa1b7
kieran nguyen <kieran.nguyen@watasolutions.com>
2021-11-24 11:26:26 +0700

    Connect to DB and adding colors to console

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -21,4 +25,4 @@ app.get('/api/product/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
