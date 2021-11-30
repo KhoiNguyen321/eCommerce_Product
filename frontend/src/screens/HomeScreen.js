5627358245c35ab5ffd02fa75e3027f8dd524cac
kieran nguyen <kieran.nguyen@watasolutions.com>
2021-11-30 17:40:27 +0700

    Fetching Products From the DB

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -20,7 +20,6 @@ const HomeScreen = () => {
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product}/>
-                            <div>asasa</div>
                     </Col>
                 ))}
            </Row>
