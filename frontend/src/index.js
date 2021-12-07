e56450808ab8905cf7ce58721ef035aee40b25c4
kieran nguyen <kieran.nguyen@watasolutions.com>
2021-12-07 19:01:23 +0700

    Action and Reducer for Product and including bringing redux state into Home Screeen useDispatch & useSelector

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import './bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
+import store from './store'
+import {Provider} from 'react-redux'
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
