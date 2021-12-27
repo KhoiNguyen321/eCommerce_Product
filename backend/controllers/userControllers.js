b1e68821178cb5f7d3e2283263d6ad4afe719204
kieran nguyen <kieran.nguyen@watasolutions.com>
2021-12-27 10:57:47 +0700

    User Registration and Password encryption

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -45,4 +45,38 @@ const getUserProfile = asyncHandler(async(req, res) => {
     }
 })
 
-export { authUser, getUserProfile}
+
+// @desc Register user 
+// @route Get /api/users/register
+// @access Private
+
+const registerUser = asyncHandler(async(req, res) => {
+    const {name, email, password} = req.body
+    const existedUser = await User.findOne({email})
+    if(existedUser){
+        res.status(400)
+        throw new Error('User already existed')
+
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+})
+
+export { authUser, getUserProfile, registerUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userControllers.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userControllers.js'
 import {protect} from '../middleware/authMiddleware.js' 
 
 
 // @desc Fetch all products
 // @route GET /api/products
 // @acess Fetch all products
-
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
