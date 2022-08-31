b87b9e204fd8a13067a61d66de39f61c0121606b
khai.vuong@watasolutions.com <khai.vuong@watasolutions.com>
2022-08-31 16:15:19 +0700

    Back end User Authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,18 @@
-import express from 'express'
-const router = express.Router()
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userControllers.js'
-import {protect} from '../middleware/authMiddleware.js' 
-
+import express from 'express';
+const router = express.Router();
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 // @desc Fetch all products
 // @route GET /api/products
 // @acess Fetch all products
-router.route('/').post(registerUser)
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
-router.route('/profile').put(protect, updateUserProfile)
-
-export default router
+router.route('/').post(registerUser);
+router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').put(protect, updateUserProfile);
+export default router;
