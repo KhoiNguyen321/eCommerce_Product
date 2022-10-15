6c2a85462fa12c7938f91156d92ec5cd45dfd090
khai.vuong@watasolutions.com <khai.vuong@watasolutions.com>
2022-10-16 05:17:24 +0700

    genearate paypal button in Order Screen

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')));
@@ -43,9 +45,6 @@ app.use((err, req, res, next) => {
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 });
-app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
 
 const PORT = process.env.PORT || 5000;
 
