14aaa9e43a57c3c0679b12f39270966879cd97ea
khai.vuong@watasolutions.com <khai.vuong@watasolutions.com>
2022-10-17 22:59:00 +0700

    fix title in HomePage

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -25,7 +25,6 @@ export const login = (email, password) => async (dispatch, getState) => {
     const config = {
       headers: { 'Content-Type': 'application/json' },
     };
-
     const { data } = await axios.post(
       '/api/users/login',
       { email, password },
